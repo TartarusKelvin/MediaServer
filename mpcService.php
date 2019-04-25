956191d49d79ecfba56c6bcb742dbab1acb06862
Nathan <tartarus9345@gmail.com>
2019-04-25 16:16:21 +0100

    Initial Commit

---
+++ mpcService.php
@@ -0,0 +1,23 @@
+<?php
+$port = " -p 6600";
+if ($_GET["command"] == "skip")
+	exec("mpc next".$port);
+if ($_GET["command"] == "status")
+	echo exec("mpc".$port);
+if ($_GET["command"] == "toggle")
+	exec ("mpc toggle".$port);
+if ($_GET["command"] == "list")
+{
+	$out = array();
+	exec("mpc lsplaylist".$port, $out);
+	foreach($out as $line) {
+		echo $line."<br>";
+	}
+}
+if ($_GET["command"] == "load"&& isset($_GET['playlist']))
+{
+	exec("mpc clear".$port);
+	echo ("mpc load ".$_GET["playlist"].$port);
+	exec ("mpc".$port." load ".$_GET["playlist"]);
+}
+?>
